2039080efb22fce98aae583d2566b54c165c2f2e
yatendra121 <yatendra121kushwaha@gmail.com>
2025-06-20 16:00:02 +0530

    fix build issue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,82 +1,94 @@
 <script setup>
-import { ref } from 'vue'
-import product3 from './../assets/img/product3-1.webp'
+import { ref } from "vue";
+import product3 from "./../assets/img/product3-1.webp";
 
-
-defineProps({
-})
-
+defineProps({});
 </script>
 
 <template>
-    <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="container">
-                    <div class="row p-5">
-                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" :src="product3" alt="">
-                        </div>
-                        <div class="col-lg-6 mb-0 d-flex align-items-center">
-                            <div class="text-align-left align-self-center">
-                                <h1 class="h1 text-success"><b>Infused Candles</b></h1>
-                                <h3 class="h2">Where luxury meets affordability</h3>
-                                <p>
-                                    These are more than just a source of light—they are a gateway to unparalleled ambiance and sophistication. Crafted with precision and care, each candle is infused with premium fragrances that transport you to serene gardens, exotic destinations, or cozy evenings by the fire.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
+      <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
+      <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="container">
+          <div class="row p-5">
+            <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+              <img class="img-fluid" :src="product3" alt="" />
             </div>
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row p-5">
-                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" :src="product3" alt="">
-                        </div>
-                        <div class="col-lg-6 mb-0 d-flex align-items-center">
-                            <div class="text-align-left">
-                                <h3 class="h2">Where luxury meets affordability</h3>
-                                <p>
-                                    You are permitted to use this Zay CSS template for your commercial websites. 
-                                    You are <strong>not permitted</strong> to re-distribute the template ZIP file in any kind of template collection websites.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-lg-6 mb-0 d-flex align-items-center">
+              <div class="text-align-left align-self-center">
+                <h1 class="h1 text-success"><b>Infused Candles</b></h1>
+                <h3 class="h2">Where luxury meets affordability</h3>
+                <p>
+                  These are more than just a source of light—they are a gateway to unparalleled
+                  ambiance and sophistication. Crafted with precision and care, each candle is
+                  infused with premium fragrances that transport you to serene gardens, exotic
+                  destinations, or cozy evenings by the fire.
+                </p>
+              </div>
             </div>
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row p-5">
-                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" src="./assets/img/banner_img_03.jpg" alt="">
-                        </div>
-                        <div class="col-lg-6 mb-0 d-flex align-items-center">
-                            <div class="text-align-left">
-                                <h1 class="h1">Repr in voluptate</h1>
-                                <h3 class="h2">Ullamco laboris nisi ut </h3>
-                                <p>
-                                    We bring you 100% free CSS templates for your websites. 
-                                    If you wish to support TemplateMo, please make a small contribution via PayPal or tell your friends about our website. Thank you.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-        <a class="carousel-control-prev text-decoration-none w-auto ps-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="prev">
-            <i class="fas fa-chevron-left"></i>
-        </a>
-        <a class="carousel-control-next text-decoration-none w-auto pe-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="next">
-            <i class="fas fa-chevron-right"></i>
-        </a>
+      </div>
+      <div class="carousel-item">
+        <div class="container">
+          <div class="row p-5">
+            <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+              <img class="img-fluid" :src="product3" alt="" />
+            </div>
+            <div class="col-lg-6 mb-0 d-flex align-items-center">
+              <div class="text-align-left">
+                <h3 class="h2">Where luxury meets affordability</h3>
+                <p>
+                  You are permitted to use this Zay CSS template for your commercial websites. You
+                  are <strong>not permitted</strong> to re-distribute the template ZIP file in any
+                  kind of template collection websites.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="container">
+          <div class="row p-5">
+            <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+              <img class="img-fluid" src="../assets/img/banner_img_03.jpg" alt="" />
+            </div>
+            <div class="col-lg-6 mb-0 d-flex align-items-center">
+              <div class="text-align-left">
+                <h1 class="h1">Repr in voluptate</h1>
+                <h3 class="h2">Ullamco laboris nisi ut</h3>
+                <p>
+                  We bring you 100% free CSS templates for your websites. If you wish to support
+                  TemplateMo, please make a small contribution via PayPal or tell your friends about
+                  our website. Thank you.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <a
+      class="carousel-control-prev text-decoration-none w-auto ps-3"
+      href="#template-mo-zay-hero-carousel"
+      role="button"
+      data-bs-slide="prev"
+    >
+      <i class="fas fa-chevron-left"></i>
+    </a>
+    <a
+      class="carousel-control-next text-decoration-none w-auto pe-3"
+      href="#template-mo-zay-hero-carousel"
+      role="button"
+      data-bs-slide="next"
+    >
+      <i class="fas fa-chevron-right"></i>
+    </a>
+  </div>
 </template>
